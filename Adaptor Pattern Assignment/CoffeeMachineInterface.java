1b593d03e9bf45ab9f137df2747d41e2a0e0b02f
anadi <sharma_anadi@ymail.com>
2021-05-10 17:01:31 -0500

    Adaptor Pattern assignment

---
+++ Adaptor Pattern Assignment/CoffeeMachineInterface.java
@@ -0,0 +1,9 @@
+//CoffeeMachineInterface.java
+
+public interface CoffeeMachineInterface {
+	public void chooseFirstSelection();
+	public void chooseSecondSelection();
+}
+
+
+
---
+++ Adaptor Pattern Assignment/CoffeeTouchscreenAdapter.java
@@ -0,0 +1,19 @@
+
+//CoffeeTouchscreenAdapter.java
+
+public class CoffeeTouchscreenAdapter implements CoffeeMachineInterface {
+
+	OldCofffeeMachine theMachine;
+
+	public CoffeeTouchscreenAdapter(OldCoffeeMachine newMachine) {
+		theMachine = newMachine;
+	}
+	
+	public void chooseFirstSelection() {
+		theMachine.selectA();
+	}
+	
+	public void chooseSecondSelection() {
+		theMachine.selectB();
+}
+}
